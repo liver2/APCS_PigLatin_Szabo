82d3f5f0b81ca256009f11e19067196eb6da3116
Liver S. <szabooli24@7hills.org>
2023-09-08 12:56:42 -0400

    beginning things & trying to index string...

---
+++ pigLatin.java
@@ -0,0 +1,19 @@
+// Liver Szabo. 9/8/2023. Pig Latin Assignment.
+import java.util.Scanner;
+
+public class pigLatin {
+    public static void main(String[] args) {
+        System.out.println("Pig Latin Translator\n"); //Intro
+        System.out.println("Please input a phrase: "); //Field for user to input
+
+        Scanner stringScan = new Scanner(System.in); //Creates a new scanner
+        String phrase = stringScan.nextLine();
+        int phraseLen = phrase.length(); // length
+        int numSpaces = 0; // used to count the number of spaces => words in phrase
+
+        // -- Count the number of spaces in the phrase. -- 
+        for (i = 0; i < phraseLen; i++) {
+            if (phrase[i] == )
+        }
+    }
+}
