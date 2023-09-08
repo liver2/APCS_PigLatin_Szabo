c2b18f04837e0bcd4bd2103a5a10d74a0cdd916e
Liver S. <szabooli24@7hills.org>
2023-09-08 13:08:54 -0400

    substringing phrase (incomplete)

--- pigLatin.java
+++ pigLatin.java
@@ -1,5 +1,6 @@
 // Liver Szabo. 9/8/2023. Pig Latin Assignment.
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class pigLatin {
     public static void main(String[] args) {
@@ -8,12 +9,27 @@ public class pigLatin {
 
         Scanner stringScan = new Scanner(System.in); //Creates a new scanner
         String phrase = stringScan.nextLine();
-        int phraseLen = phrase.length(); // length
-        int numSpaces = 0; // used to count the number of spaces => words in phrase
+        int phraseLen = phrase.length(); //length
+        int numSpaces = 0; //used to count the number of spaces => words in phrase
+        ArrayList<Integer> spacePosition = new ArrayList<Integer>();
 
         // -- Count the number of spaces in the phrase. -- 
-        for (i = 0; i < phraseLen; i++) {
-            if (phrase[i] == )
+        for (int i = 0; i < phraseLen; i++) { //looping through phrase individual characters
+            if (phrase[i].equals(" ")) {
+                spacePosition.set(numSpaces, i); //add a space position marker at i. CAREFUL, index starts at zero i think.
+                numSpaces++; //count up number of spaces
+            }
+        }
+
+        int numWords = (numSpaces + 1); //number of words will be important for the next array: words[]
+
+        String words[] = new String[numWords]; //make a new array words to store words. we will use this array and turn these words into piglat
+        int spacePositionCursor = -1; //setting this to negative 1 so that the substring index will start at 0.
+
+        for (int j = 0; j < numSpaces; j++) { //substring with loop
+            words[j] = phrase.substring(spacePositionCursor + 1, spacePosition.get(j));
+            spacePositionCursor = spacePosition.get(j);
+
         }
     }
 }
